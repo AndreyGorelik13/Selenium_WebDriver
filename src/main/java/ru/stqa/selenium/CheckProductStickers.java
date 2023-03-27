eaa532e7667a0b5444adb122945acc9c213498a1
Горелик  Андрей <Andrey.Gorelik@fors.ru>
2023-03-27 21:20:53 +0300

    Задание 7. Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ src/main/java/ru/stqa/selenium/CheckProductStickers.java
@@ -0,0 +1,44 @@
+package ru.stqa.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class CheckProductStickers {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void checkProductStickersTest() {
+        driver.get("http://localhost/litecart");
+
+        List<WebElement> productsList = driver.findElements(By.cssSelector("li.product"));
+        for (WebElement product : productsList) {
+
+            List<WebElement> stickersList = product.findElements(By.cssSelector("div.sticker"));
+            Assert.assertEquals(1, stickersList.size());
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
